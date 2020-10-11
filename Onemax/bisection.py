f4b7315ec6d1f94142c9a31c00f4c8b0009c0d23
PhamLeQuangNhat <18520120@gm.uit.edu.vn>
2020-10-11 11:49:49 +0700

    Loading 100%

--- Onemax/bisection.py
+++ Onemax/bisection.py
@@ -1,40 +1,96 @@
+from Onemax import OneMaxProblem
+import numpy as np 
+
 class Bisection:
-
-def run_multiple_times(seed, times, _type, population_size, problem_size, tournament_size):
-    evals = 0
-    for i in range(times):
-        result = True
-        number_of_evaluations = 0 
-        #x = 0
-        print("number_of_evaluations before " + str(i)+ "th = {}".format(number_of_evaluations))
-        print("Maximizing with random_seed = {}".format(seed))
-        #Problem = OneMaxProblem(seed, _type, N, problem_size,tournament_size,number_of_evaluations)
-        number_of_evaluations, population = OneMaxProblem(seed, _type, population_size, problem_size, tournament_size, number_of_evaluations)
-        #print("number_of_evaluations = {}".format(number_of_evaluations))
-        print("number_of_evaluations after " + str(i)+ "th = {}".format(number_of_evaluations))
-        print("Population = {}".format(population))
-        population = np.unique(population)
-        print("Population = {}".format(population))
-        print("Len population = {}".format(len(population)))
-        if len(population) != 1:
-            return 0, False
-        evals += number_of_evaluations
-        seed += 1
-
-def get_MRPS_upper_bound(seed, _type, problem_size, tournament_size):
-    N_upper = 4
-    times = 10
-    evals, success = run_multiple_times(seed, times, _type, N_upper, problem_size, tournament_size)
-
-    while (not success ):
-        N_upper = N_upper * 2
-        if N_upper <= 8192:
+    def __init__(self, times, seed, type_problem, type_crossover, problem_size,tournament_size):
+        self.times = times
+        self.seed = seed
+        self.type_problem = type_problem
+        self.type_crossover = type_crossover
+        self.problem_size = problem_size
+        self.tournament_size = tournament_size
         
-            print("Running N_upper: {}".format(N_upper))
+    def run_multiple_times(self, population_size):
+        sum_evals = 0
+        seed = self.seed
+        for i in range(self.times):
 
-            evals, success = run_multiple_times(seed, times, _type, N_upper, problem_size, tournament_size)
-            print("success: {}".foamat(success))
-        else:
-            return 0,0, False
-    return N_upper, evals, success
+            number_of_evaluations = 0 
+            
+            print("number_of_evaluations before " + str(i)+ "th = {}".format(number_of_evaluations))
+            print("Maximizing with random_seed = {}".format(seed))
+            #Problem = OneMaxProblem(seed, _type, N, problem_size,tournament_size,number_of_evaluations)
+            Problem = OneMaxProblem(seed, self.type_problem, self.type_crossover, population_size,
+                                     self.problem_size, self.tournament_size, number_of_evaluations)
+            number_of_evaluations, population = Problem.run()
+            #print("number_of_evaluations = {}".format(number_of_evaluations))
+            print("number_of_evaluations after " + str(i)+ "th = {}".format(number_of_evaluations))
+            #print("Population = {}".format(population))
+            population = np.unique(population)
+            #print("Population = {}".format(population))
+            #print("Len population = {}".format(len(population)))
+            if len(population) != 1:
+                return 0, False
+            sum_evals += number_of_evaluations
+            seed += 1
+        return sum_evals/self.times, True
 
+    def get_MRPS_upper_bound(self):
+        N_upper = 4
+        
+        evals, success = self.run_multiple_times(N_upper)
+
+        while (not success ):
+            N_upper = N_upper * 2
+            if N_upper <= 8192:
+            
+                print("Running N_upper: {}".format(N_upper))
+
+                evals, success = self.run_multiple_times(N_upper)
+
+                print("success: {}".format(success))
+            else:
+                return 0,0, False
+        print("SEED : {}".format(self.seed))
+        return N_upper, evals, success
+    
+    def find_MRPS(self,N_upper, evals_upper):
+        evals = evals_upper
+        N_lower = N_upper // 2
+        while (N_upper - N_lower)/N_upper > 0.1:
+            N = (N_upper + N_lower) // 2
+            
+            x, success = self.run_multiple_times(N)
+
+            print(f'  N_upper: {N_upper} - N_lower: {N_lower} - success: {success} - x: {x}')
+            if success:
+                N_upper = N
+                evals = x
+            else:
+                N_lower = N
+            
+            if (N_upper - N_lower) <= 2:
+                
+                return evals, N_upper
+            
+        return evals, N_upper
+
+    def run(self):
+        N_upper, evals_upper, success = self.get_MRPS_upper_bound()
+        #print(N_upper,evals_upper,success)
+        
+        if not success:
+             return 0, 0, False
+        evals, MRPS = self.find_MRPS(N_upper, evals_upper)
+        
+        return evals, MRPS, True
+
+"""
+if __name__ == "__main__":
+    bi =  Bisection(10,18520120,"OneMax","1X",80,4)
+    bi.run()
+
+    #N_upper, evals, success = bi.get_MRPS_upper_bound()
+    evals, MRPS, success = bi.run()
+    print(evals, MRPS, success)
+"""
